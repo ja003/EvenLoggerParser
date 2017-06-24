0b56ee2ab0b9a4fac0ba04d00366691676e74f56
Adam Jurík <ja004@seznam.cz>
2017-06-24 15:28:44 +0200

    Přidejte soubory projektu.

---
+++ EvenLoggerParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvenLoggerParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
