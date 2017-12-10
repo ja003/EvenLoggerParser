00028de90344984705c5cf682cd7bfb8c70449dc
Adam Jurík <ja004@seznam.cz>
2017-12-10 20:45:07 +0100

    Average time threshold

--- EventLoggerParser/EventRecordAnalyzer.cs
+++ EventLoggerParser/EventRecordAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace EvenLoggerParser
 {
@@ -31,10 +32,20 @@ namespace EvenLoggerParser
 			return count;
 		}
 
-		public static DateTime GetAverageTime(string pName, List<EventRecord> pRecords)
+		public static DateTime GetAverageTime(string pName, List<EventRecord> pRecords, DateTime? pAverageTime = null)
 		{
 			int count = 0;
 			long ticks = 0;
+			if (pAverageTime == null)
+			{
+				pAverageTime = new DateTime(1, 1, 1, 0, 0, 0);
+			}
+			else
+			{
+				pAverageTime = new DateTime(1, 1, 1, 
+					pAverageTime.Value.Hour, pAverageTime.Value.Minute, pAverageTime.Value.Second);
+			}
+
 			foreach(EventRecord er in pRecords)
 			{
 				if(er.name == pName)
@@ -42,8 +53,19 @@ namespace EvenLoggerParser
 					count++;
 					EventRecord copy = er.Clone();
 					copy.dateTime = new DateTime(1, 1, 1, copy.dateTime.Hour, copy.dateTime.Minute, copy.dateTime.Second);
-					ticks += copy.dateTime.Ticks;
+					long erTicks = copy.dateTime.Ticks;
+					if (erTicks < pAverageTime.Value.Ticks &&
+					    (pAverageTime.Value.Ticks - erTicks) > pAverageTime.Value.Ticks / 2)
+					{
+						erTicks += pAverageTime.Value.Ticks;
+					}
+					ticks += erTicks;
 				}
+			}
+			if (count == 0)
+			{
+				Console.WriteLine("No records of " + pName + " found.");
+				return DateTime.MaxValue;
 			}
 			DateTime dateTime = new DateTime(ticks / count);
 
--- EventLoggerParser/Program.cs
+++ EventLoggerParser/Program.cs
@@ -10,6 +10,7 @@ namespace EvenLoggerParser
 		{
 			EventRecordParser parser = new EventRecordParser();
 			List<EventRecord> records = parser.ParseFile(
+				//"D:\\ja004\\Dropbox\\Project\\coding\\EvenLoggerParser\\events_test.txt");
 				"D:\\ja004\\Dropbox\\Project\\coding\\EvenLoggerParser\\events_2017_12_10 - 2017_01_11.txt");
 
 			/*int mCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Masturbation, records);
@@ -23,13 +24,26 @@ namespace EvenLoggerParser
 			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records);
 			*/
 
-			int sexCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records);
+			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 23, 59, 59));
+			//TODO: make weighted average
+			DateTime sleepAverage22 = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 22, 59, 59));
+			DateTime sleepAverage21 = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 21, 59, 59));
+			DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime(EventCode.WakeUp, records);
+			//zde upřesnit čas netřeba (udělá malý rozdíl)
+			//DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime(EventCode.WakeUp, records, new DateTime(1, 1, 1, 8, 0, 0));
+
+			int sexTerkaCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records, new DateTime(2017, 6, 1));
+
+			int mCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Masturbation, records);
+			DateTime mAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Masturbation, records);
+
+			/*int sexCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records);
 			int sexTerkaCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records, new DateTime(2017, 6, 1));
 
 
 			int sportCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sport, records);
 			int posilovaniCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Posilovani, records);
-
+			*/
 			Console.ReadLine();
 		}
 	}
