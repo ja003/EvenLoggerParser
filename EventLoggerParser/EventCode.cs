f4962ba04405329f196f1857ab2a3bae0a564ba3
Adam Jurík <ja004@seznam.cz>
2017-12-10 19:38:06 +0100

    Event codes

---
+++ EventLoggerParser/EventCode.cs
@@ -0,0 +1,11 @@
+﻿namespace EvenLoggerParser
+{
+	public static class EventCode
+	{
+		public static string Sleep = "Sleep";
+		public static string WakeUp = "Wake";
+		public static string Masturbation = "M";
+		public static string Poop = "P";
+		public static string Sport = "Sport";
+	}
+}
--- EventLoggerParser/Program.cs
+++ EventLoggerParser/Program.cs
@@ -12,16 +12,18 @@ namespace EvenLoggerParser
 			List<EventRecord> records = EventRecordParser.ParseFile(
 				"D:\\ja004\\Dropbox\\Project\\coding\\EvenLoggerParser\\events_2017_12_10 - 2017_01_11.txt");
 
-			int mCount = EventRecordAnalyzer.GetEventRecordCount("M", records);
-			DateTime mAverage = EventRecordAnalyzer.GetAverageTime("M", records);
+			/*int mCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Masturbation, records);
+			DateTime mAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Masturbation, records);
 
 
-			int wakeCount = EventRecordAnalyzer.GetEventRecordCount("Wake", records);
-			DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime("Wake", records);
+			int wakeCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.WakeUp, records);
+			DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime(EventCode.WakeUp, records);
 
-			int sleepCount = EventRecordAnalyzer.GetEventRecordCount("Sleep", records);
-			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime("Sleep", records);
+			int sleepCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sleep, records);
+			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records);
+			*/
 
+			int sportCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sport, records);
 
 			Console.ReadLine();
 		}
