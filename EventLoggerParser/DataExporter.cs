e7a7574d2524a5da604dc71e62eb3a7f264274cb
Adam Jurík <ja004@seznam.cz>
2017-12-28 14:06:05 +0100

    refaktor

--- EventLoggerParser/DataExporter.cs
+++ EventLoggerParser/DataExporter.cs
@@ -6,14 +6,15 @@ namespace EventLoggerParser
 {
 	public static class DataExporter
 	{
-		public static void ExportRecords(List<EventRecord> pRecords)
+		public static void ExportRecords(List<EventRecord> pRecords, string pFolderName = "")
 		{
 			EventRecord firstRecord = pRecords[0];
 			string solutionPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
-			string folder = "\\records\\" + firstRecord.Name;
+			string subfolder = pFolderName.Length == 0 ? firstRecord.Name : pFolderName;
+			string folder = "\\records\\" + subfolder;
 			//string fullFilePath = solutionPath + "\\records\\" + firstRecord.name + "_" + DateTime.Now.ToString("hh:mm:ss") + ".txt";
-			string fullFilePath = Path.Combine(solutionPath + folder, firstRecord.Name + "-"  + DateTime.Now.ToString("yyyy_MM_dd") + "-" + DateTime.Now.ToString("hh_mm_ss") + ".txt");
-			Console.WriteLine("exporting to : " + fullFilePath);
+			string fullFilePath = Path.Combine(solutionPath + folder, subfolder + "-"  + DateTime.Now.ToString("yyyy_MM_dd") + "-" + DateTime.Now.ToString("hh_mm_ss") + ".txt");
+			Console.WriteLine(pFolderName + " exporting to : " + fullFilePath);
 
 			//System.IO.File.CreateText(file);
 			using(System.IO.StreamWriter file = new System.IO.StreamWriter(@fullFilePath))
--- EventLoggerParser/Program.cs
+++ EventLoggerParser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace EventLoggerParser
 {
@@ -26,34 +28,50 @@ namespace EventLoggerParser
 			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records);
 			*/
 
-			List<EventRecord> poop = parser.ExtractRecordsOfName(records, EventCode.Poop);
-			Console.WriteLine("exporting num of poop = " + poop.Count);
-			DataExporter.ExportRecords(poop);
+			bool export = false;
+			if (export)
+			{
+				List<EventRecord> poop = parser.ExtractRecordsOfName(records, EventCode.Poop);
+				List<EventRecord> masturbation = parser.ExtractRecordsOfName(records, EventCode.Masturbation);
+				List<EventRecord> sleep = parser.ExtractRecordsOfName(records, EventCode.Sleep);
+				List<EventRecord> wakeUp = parser.ExtractRecordsOfName(records, EventCode.WakeUp);
 
-			List<EventRecord> masturbation = parser.ExtractRecordsOfName(records, EventCode.Masturbation);
-			Console.WriteLine("exporting num of masturbation = " + masturbation.Count);
-			DataExporter.ExportRecords(masturbation);
+				IEnumerable<EventRecord> allTogether = poop.Concat(masturbation).Concat(sleep).Concat(wakeUp);
+				List<EventRecord> allTogetherList = allTogether.ToList();
 
+				Console.WriteLine("exporting num of poop = " + poop.Count);
+				DataExporter.ExportRecords(poop);
+				Console.WriteLine("exporting num of sleep = " + sleep.Count);
+				DataExporter.ExportRecords(sleep);
+				Console.WriteLine("exporting num of masturbation = " + masturbation.Count);
+				DataExporter.ExportRecords(masturbation);
+				Console.WriteLine("exporting num of wakeUp = " + wakeUp.Count);
+				DataExporter.ExportRecords(wakeUp);
+				DataExporter.ExportRecords(allTogetherList, "All");
+			}
+
+			/*
 			DateTime sleepAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 23, 59, 59));
 			//TODO: make weighted average
 			DateTime sleepAverage22 = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 22, 59, 59));
 			DateTime sleepAverage21 = EventRecordAnalyzer.GetAverageTime(EventCode.Sleep, records, new DateTime(1, 1, 1, 21, 59, 59));
 			DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime(EventCode.WakeUp, records);
+			*/
 			//zde upřesnit čas netřeba (udělá malý rozdíl)
 			//DateTime wakeAverage = EventRecordAnalyzer.GetAverageTime(EventCode.WakeUp, records, new DateTime(1, 1, 1, 8, 0, 0));
 
+
+			int sexCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records);
 			int sexTerkaCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records, new DateTime(2017, 6, 1));
 
 			int mCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Masturbation, records);
-			DateTime mAverage = EventRecordAnalyzer.GetAverageTime(EventCode.Masturbation, records);
-
-			/*int sexCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records);
-			int sexTerkaCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sex, records, new DateTime(2017, 6, 1));
-
+			int poopCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Poop, records);
+			int sleepCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sleep, records);
+			int wakeUpCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.WakeUp, records);
 
 			int sportCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Sport, records);
 			int posilovaniCount = EventRecordAnalyzer.GetEventRecordCount(EventCode.Posilovani, records);
-			*/
+			
 			Console.ReadLine();
 		}
 	}
